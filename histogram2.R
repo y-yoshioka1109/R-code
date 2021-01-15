53f646c13f8b1b1b5d1e11544e90f6e1019fc3e6
y-yoshioka1109 <69947704+y-yoshioka1109@users.noreply.github.com>
2021-01-15 15:30:23 +0900

    Update histogram2.R

--- histogram2.R
+++ histogram2.R
@@ -1,4 +1,4 @@
-df <- read.delim("hist", header=T)
+df <- read.delim("hist2", header=T)
 g <- NULL
 g <- ggplot(df, aes(x = Value, fill=Class))
 g <- g + geom_histogram(bins = 100,alpha=0.6,position="identity")
