bfc6ae9c21297c7d2cab94645cf7f4537545a810
Yuki Yoshioka <69947704+y-yoshioka1109@users.noreply.github.com>
2021-02-03 17:07:48 +0900

    Update histogram2.R

--- histogram2.R
+++ histogram2.R
@@ -4,3 +4,9 @@ g <- NULL
 g <- ggplot(df, aes(x = Value, fill=Class))
 g <- g + geom_histogram(bins = 100,alpha=0.6,position="identity")
 g
+
+#折れ線で表す場合はこっち
+g <- NULL
+g <- ggplot(df, aes(x = Value, colour=Class))
+g <- g + geom_freqpoly(binwidth=2)
+g
