f5b3c17d96eecf0b743e5dc7ddcaa70bf6257da0
Yuki Yoshioka <69947704+y-yoshioka1109@users.noreply.github.com>
2021-02-23 15:02:02 +0900

    Update cumulative_bar-plot.R

--- cumulative_bar-plot.R
+++ cumulative_bar-plot.R
@@ -4,4 +4,5 @@ df <- read.delim("hit3.txt",header=T)
 g <- ggplot(df, aes(x = Class, y = Value, fill = Name))
 g <- g + geom_bar(stat = "identity")
 #g <- g + theme(legend.position = 'none')#判例に表示される数が多くなる場合は削除する
+#g <- g + theme(axis.text.x = element_text(angle = 30, hjust = 1)) #x軸のラベルを斜めにする
 g
